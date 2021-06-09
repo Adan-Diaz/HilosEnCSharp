2b2693962a7a4a92c995bc046e79c2bc575541dc
Adan-Diaz <83990436+Adan-Diaz@users.noreply.github.com>
2021-06-08 19:34:33 -0500

    Agregar archivos de proyecto.

---
+++ HilosEnCSharp/HilosEnCSharp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HilosEnCSharp
+{
+    class HilosEnCSharp
+    {
+        public void sumarHilo(object parametros)
+        {
+            Thread current = Thread.CurrentThread;
+            Int32[] numeros = (Int32[])parametros;
+            Int32 resultado = numeros[0] + numeros[1];
+            Console.WriteLine("El hilo " + current.Name + ", durmiendo por "
+            + resultado + "segundos");
+            Thread.Sleep(resultado * 1000);
+            Console.WriteLine("El resultado de la suma de " + numeros[0] + " + " + numeros[1] + " es: "
+                 + resultado);
+            Console.WriteLine("hilo " + current.Name + " finalizo su trabajo");
+           }
+    }
+}
---
+++ HilosEnCSharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HilosEnCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HilosEnCSharp ejemplo = new HilosEnCSharp();
+            Thread hilo1 = new Thread(new ParameterizedThreadStart(ejemplo.sumarHilo));
+            Thread hilo2 = new Thread(new ParameterizedThreadStart(ejemplo.sumarHilo));
+            Thread hilo3 = new Thread(new ParameterizedThreadStart(ejemplo.sumarHilo));
+
+            hilo1.Name = "Hilo1";
+            hilo2.Name = "Hilo2";
+            hilo3.Name = "Hilo3";
+
+            Int32[] parametros1 = { 1, 2 };
+            Int32[] parametros2 = { 2, 3 };
+            Int32[] parametros3 = { 3, 4 };
+
+            Console.WriteLine("Preparando los hilos");
+
+            hilo1.Start(parametros1);
+            hilo2.Start(parametros2);
+            hilo3.Start(parametros3);
+            Console.WriteLine("Hilos Procesando.....");
+            Console.ReadLine();
+
+        }
+    }
+}
